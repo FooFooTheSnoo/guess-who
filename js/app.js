3332e6bb2fc88470964485ce4d29b73f91e82a8c
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-02-09 21:06:41 -0800

    edited functions to have multi answers

--- js/app.js
+++ js/app.js
@@ -11,11 +11,13 @@ user();
 function firstQuestion() {
   let firstQuestion = prompt('My name is Reginald. (Y/N)').toUpperCase();
 
-  if (firstQuestion === 'Y') {
+  if (firstQuestion === 'Y' || firstQuestion === 'YES') {
     alert('My name is not Reginald, it is Josh!');
     // console.log('My name is not Reginald, it is Josh!');
-  } else if (firstQuestion === 'N') {
+  } else if (firstQuestion === 'N' || firstQuestion === 'NO') {
     alert('My name is Josh!');
+  } else {
+    alert('Please type y/n');
   }
 }
 firstQuestion();
@@ -25,11 +27,13 @@ function secondQuestion() {
     'Josh used to work at a sushi bar makin damn delicious sushi (Y/N)'
   ).toUpperCase();
 
-  if (secondQuestion === 'Y') {
+  if (secondQuestion === 'Y' || secondQuestion === 'YES') {
     alert('He wishes he knew how to make good sushi');
     // console.log(secondQuestion());
-  } else if (secondQuestion === 'N') {
+  } else if (secondQuestion === 'N' || secondQuestion === 'NO') {
     alert('Josh has no clue how to make sushi');
+  } else {
+    alert('Please type y/n');
   }
 }
 secondQuestion();
@@ -40,13 +44,15 @@ function thirdQuestion() {
     'Josh really wants to become a gardener after he finishes this coding course (Y/N)'
   ).toUpperCase();
 
-  if (thirdQuestion === 'Y') {
+  if (thirdQuestion === 'Y' || thirdQuestion === 'YES') {
     alert('He would prefer to get a job utilizing his newfound skillsets');
     // console.log(thirdQuestion());
-  } else if (thirdQuestion === 'N') {
+  } else if (thirdQuestion === 'N' || thirdQuestion === 'NO') {
     alert(
       'He would totally pick up gardening once he is working remote 35 hrs/wk makin 120k'
     );
+  } else {
+    alert('Please type y/n');
   }
 }
 thirdQuestion();
@@ -57,11 +63,13 @@ function fourthQuestion() {
     'One of Josh\'s goals is to get enough experience to get a Software Engineer work title'
   ).toUpperCase();
 
-  if (fourthQuestion === 'Y') {
+  if (fourthQuestion === 'Y' || fourthQuestion === 'YES') {
     alert('Golly gee, he would love to earn that title pretty soon');
     // console.log(fourthQuestion());
-  } else if (fourthQuestion === 'N') {
+  } else if (fourthQuestion === 'N' || fourthQuestion === 'NO') {
     alert('He would also settle for a Seinor Sofware Engineer');
+  } else {
+    alert('Please type y/n');
   }
 }
 fourthQuestion();
@@ -70,11 +78,13 @@ fourthQuestion();
 function fifthQuestion() {
   let fifthQuestion = prompt('Do you think Josh likes pizza?').toUpperCase();
 
-  if (fifthQuestion === 'Y') {
+  if (fifthQuestion === 'Y' || fifthQuestion === 'YES') {
     alert('Of course he likes pizza! Who does not?');
     // console.log(fifthQuestion());
-  } else if (fifthQuestion === 'N') {
+  } else if (fifthQuestion === 'N' || fifthQuestion === 'NO') {
     alert('Of course he likes pizza!');
+  } else {
+    alert('Please type y/n');
   }
 }
 fifthQuestion();
