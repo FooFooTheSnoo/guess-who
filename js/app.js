c83046046bb2192b5da11b0325de75a8b170d190
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-02-11 14:36:52 -0800

    added top10 list and work bullets to HTML

--- js/app.js
+++ js/app.js
@@ -1,91 +1,93 @@
 'use strict';
 
-function user() {
-  let user = prompt('Hello! What is your name?');
-
-  alert('Thank you for visiting my website, ' + user + '!');
-  document.write('Thank you for visiting my website, ' + user + '!');
-}
-user();
-
-function firstQuestion() {
-  let firstQuestion = prompt('My name is Reginald. (Y/N)').toUpperCase();
-
-  if (firstQuestion === 'Y' || firstQuestion === 'YES') {
-    alert('My name is not Reginald, it is Josh!');
-    // console.log('My name is not Reginald, it is Josh!');
-  } else if (firstQuestion === 'N' || firstQuestion === 'NO') {
-    alert('My name is Josh!');
-  } else {
-    alert('Please type y/n');
-  }
-}
-firstQuestion();
-
-function secondQuestion() {
-  let secondQuestion = prompt(
-    'Josh used to work at a sushi bar makin damn delicious sushi (Y/N)'
-  ).toUpperCase();
-
-  if (secondQuestion === 'Y' || secondQuestion === 'YES') {
-    alert('He wishes he knew how to make good sushi');
-    // console.log(secondQuestion());
-  } else if (secondQuestion === 'N' || secondQuestion === 'NO') {
-    alert('Josh has no clue how to make sushi');
-  } else {
-    alert('Please type y/n');
-  }
-}
-secondQuestion();
 
 
-function thirdQuestion() {
-  let thirdQuestion = prompt(
-    'Josh really wants to become a gardener after he finishes this coding course (Y/N)'
-  ).toUpperCase();
+// function user() {
+//   let user = prompt('Hello! What is your name?');
 
-  if (thirdQuestion === 'Y' || thirdQuestion === 'YES') {
-    alert('He would prefer to get a job utilizing his newfound skillsets');
-    // console.log(thirdQuestion());
-  } else if (thirdQuestion === 'N' || thirdQuestion === 'NO') {
-    alert(
-      'He would totally pick up gardening once he is working remote 35 hrs/wk makin 120k'
-    );
-  } else {
-    alert('Please type y/n');
-  }
-}
-thirdQuestion();
+//   alert('Thank you for visiting my website, ' + user + '!');
+//   document.write('Thank you for visiting my website, ' + user + '!');
+// }
+// user();
+
+// function firstQuestion() {
+//   let firstQuestion = prompt('My name is Reginald. (Y/N)').toUpperCase();
+
+//   if (firstQuestion === 'Y' || firstQuestion === 'YES') {
+//     alert('My name is not Reginald, it is Josh!');
+//     // console.log('My name is not Reginald, it is Josh!');
+//   } else if (firstQuestion === 'N' || firstQuestion === 'NO') {
+//     alert('My name is Josh!');
+//   } else {
+//     alert('Please type y/n');
+//   }
+// }
+// firstQuestion();
+
+// function secondQuestion() {
+//   let secondQuestion = prompt(
+//     'Josh used to work at a sushi bar makin damn delicious sushi (Y/N)'
+//   ).toUpperCase();
+
+//   if (secondQuestion === 'Y' || secondQuestion === 'YES') {
+//     alert('He wishes he knew how to make good sushi');
+//     // console.log(secondQuestion());
+//   } else if (secondQuestion === 'N' || secondQuestion === 'NO') {
+//     alert('Josh has no clue how to make sushi');
+//   } else {
+//     alert('Please type y/n');
+//   }
+// }
+// secondQuestion();
 
 
-function fourthQuestion() {
-  let fourthQuestion = prompt(
-    'One of Josh\'s goals is to get enough experience to get a Software Engineer work title'
-  ).toUpperCase();
+// function thirdQuestion() {
+//   let thirdQuestion = prompt(
+//     'Josh really wants to become a gardener after he finishes this coding course (Y/N)'
+//   ).toUpperCase();
 
-  if (fourthQuestion === 'Y' || fourthQuestion === 'YES') {
-    alert('Golly gee, he would love to earn that title pretty soon');
-    // console.log(fourthQuestion());
-  } else if (fourthQuestion === 'N' || fourthQuestion === 'NO') {
-    alert('He would also settle for a Seinor Sofware Engineer');
-  } else {
-    alert('Please type y/n');
-  }
-}
-fourthQuestion();
+//   if (thirdQuestion === 'Y' || thirdQuestion === 'YES') {
+//     alert('He would prefer to get a job utilizing his newfound skillsets');
+//     // console.log(thirdQuestion());
+//   } else if (thirdQuestion === 'N' || thirdQuestion === 'NO') {
+//     alert(
+//       'He would totally pick up gardening once he is working remote 35 hrs/wk makin 120k'
+//     );
+//   } else {
+//     alert('Please type y/n');
+//   }
+// }
+// thirdQuestion();
 
 
-function fifthQuestion() {
-  let fifthQuestion = prompt('Do you think Josh likes pizza?').toUpperCase();
+// function fourthQuestion() {
+//   let fourthQuestion = prompt(
+//     'One of Josh\'s goals is to get enough experience to get a Software Engineer work title'
+//   ).toUpperCase();
 
-  if (fifthQuestion === 'Y' || fifthQuestion === 'YES') {
-    alert('Of course he likes pizza! Who does not?');
-    // console.log(fifthQuestion());
-  } else if (fifthQuestion === 'N' || fifthQuestion === 'NO') {
-    alert('Of course he likes pizza!');
-  } else {
-    alert('Please type y/n');
-  }
-}
-fifthQuestion();
+//   if (fourthQuestion === 'Y' || fourthQuestion === 'YES') {
+//     alert('Golly gee, he would love to earn that title pretty soon');
+//     // console.log(fourthQuestion());
+//   } else if (fourthQuestion === 'N' || fourthQuestion === 'NO') {
+//     alert('He would also settle for a Seinor Sofware Engineer');
+//   } else {
+//     alert('Please type y/n');
+//   }
+// }
+// fourthQuestion();
+
+
+// function fifthQuestion() {
+//   let fifthQuestion = prompt('Do you think Josh likes pizza?').toUpperCase();
+
+//   if (fifthQuestion === 'Y' || fifthQuestion === 'YES') {
+//     alert('Of course he likes pizza! Who does not?');
+//     // console.log(fifthQuestion());
+//   } else if (fifthQuestion === 'N' || fifthQuestion === 'NO') {
+//     alert('Of course he likes pizza!');
+//   } else {
+//     alert('Please type y/n');
+//   }
+// }
+// fifthQuestion();
 
