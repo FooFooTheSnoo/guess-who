7d625dde9c4b2291139a206099a3f3498c7a21d3
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-02-13 22:00:50 -0800

    added travel guessing question in js.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 'use strict';
+let score = 0;
 
 function user() {
   let user = prompt('Hello! What is your name?');
 
   alert('Thank you for visiting my website, ' + user + '!');
-  document.write('Thank you for visiting my website, ' + user + '!');
+  document.write(
+    'Thank you for visiting my website, ' + user + '! Your score is ' + score
+  );
 }
-user();
 
 function firstQuestion() {
   let firstQuestion = prompt('My name is Reginald. (Y/N)').toUpperCase();
@@ -32,6 +34,7 @@ function secondQuestion() {
     // console.log(secondQuestion());
   } else if (secondQuestion === 'N' || secondQuestion === 'NO') {
     alert('Josh has no clue how to make sushi');
+    score++;
   } else {
     alert('Please type y/n');
   }
@@ -46,6 +49,7 @@ function thirdQuestion() {
   if (thirdQuestion === 'Y' || thirdQuestion === 'YES') {
     alert('He would prefer to get a job utilizing his newfound skillsets');
     // console.log(thirdQuestion());
+    score++;
   } else if (thirdQuestion === 'N' || thirdQuestion === 'NO') {
     alert(
       'He would totally pick up gardening once he is working remote 35 hrs/wk makin 120k'
@@ -58,12 +62,13 @@ thirdQuestion();
 
 function fourthQuestion() {
   let fourthQuestion = prompt(
-    "One of Joshs goals is to get enough experience to get a Software Engineer work title"
+    'One of Joshs goals is to get enough experience to get a Software Engineer work title'
   ).toUpperCase();
 
   if (fourthQuestion === 'Y' || fourthQuestion === 'YES') {
     alert('Golly gee, he would love to earn that title pretty soon');
     // console.log(fourthQuestion());
+    score++;
   } else if (fourthQuestion === 'N' || fourthQuestion === 'NO') {
     alert('He would also settle for a Seinor Sofware Engineer');
   } else {
@@ -78,6 +83,7 @@ function fifthQuestion() {
   if (fifthQuestion === 'Y' || fifthQuestion === 'YES') {
     alert('Of course he likes pizza! Who does not?');
     // console.log(fifthQuestion());
+    score++;
   } else if (fifthQuestion === 'N' || fifthQuestion === 'NO') {
     alert('Of course he likes pizza!');
   } else {
@@ -92,7 +98,7 @@ let userGuess = prompt(
   "I bet you cant guess what number I'm thinking of between 1-10"
 );
 userGuess = Number(userGuess);
-while (attempt > 0) {
+while (attempt > 0)
   if (userGuess < guessNumber) {
     userGuess = prompt('Thats a little low! Try again!');
     userGuess = Number(userGuess);
@@ -101,42 +107,15 @@ while (attempt > 0) {
     userGuess = Number(userGuess);
   } else if (userGuess === guessNumber) {
     alert('Thats correct! Nice job!');
+    score++;
     break;
   }
-  attempt = attempt - 1;
-  if (!attempt) {
-    alert('You have run out of guesses!');
-  }
+attempt = attempt - 1;
+if (!attempt) {
+  alert('You have run out of guesses!');
 }
 
-let travelTo = ['germany', 'japan', 'switzerland', 'nova scotia'];
-let attempts = 5;
-let userGuess2 = prompt('Guess what a country I want to travel to is?');
-let guessCorrect = false;
-
-while (attempts > 0) {
-  for (let i = 0; i < travelTo.length; i++) {
-    let country = travelTo[i];
-    console.log(userGuess, country);
-    if (userGuess.toLowerCase() === country.toLowerCase());
-    alert('Thats right! I would love to go there!');
-    guessCorrect = true;
-    break;
-  }
-  if (guessCorrect) break;
-
-  attempts = attempts - 1;
-  if (!attempts) {
-    alert('Nice try! Maybe you will get one next time!');
-  } else {
-    userGuess = prompt(
-      "That sounds like an awesome place! But it's not one on my list"
-    );
-  }
-}
-
-let 
-
+// }
 // make a loop that gives the user up to 4 guesses to
 // guess correct answer which is called guess number
 
@@ -148,3 +127,28 @@ let
 // alert user if out of guesses
 
 // console.log(userguess, currentplace)
+
+let userAttempts = 6;
+let myAnswers = ['germany', 'japan', 'switzerland', 'spain', 'france', 'china'];
+//while loop controls the game
+while (userAttempts > 0) {
+  let userResponse = prompt(
+    `Guess one coutries I would like to go visit. ${userAttempts} tries!`
+  );
+  userAttempts--;
+
+  for (let i = 0; i < myAnswers.length; i++) {
+    if (myAnswers[i] === userResponse) {
+      alert('Yes! Definitely on my list of places to go!');
+      userAttempts = 0;
+      score++;
+      break;
+    }
+  }
+  if (!userAttempts) {
+    alert(
+      'You have run out of tries! I would like to go to Germany, Japan, France, and Switzerland'
+    );
+  }
+}
+user();
