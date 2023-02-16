0568e4586a567cfd0d48902af144ab8b437945e2
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-02-15 20:27:41 -0800

    js file update function

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 "use strict";
-// let score = 0;
+let score = 0;
 
-// function user() {
-//   let user = prompt("Hello! What is your name?");
+function user() {
+  let user = prompt("Hello! What is your name?");
 
-//   alert("Thank you for visiting my website, " + user + "!");
-//   document.write(
-//     "Thank you for visiting my website, " + user + "! Your score is " + score
-//   );
-// }
-
-//julio code
+  alert("Thank you for visiting my website, " + user + "!");
+  document.write(
+    "Thank you for visiting my website, " + user + "! Your score is " + score
+  );
+}
 
 function questions(question, correctAnswer, wrongAnswer) {
   let answer = prompt(question).toUpperCase();
   if (answer === "Y" || answer === "YES") {
     alert(correctAnswer);
+    score++;
+    console.log("Your current score is -->", score);
   } else if (answer === "N" || answer === "NO") {
     alert(wrongAnswer);
   } else {
@@ -28,90 +28,26 @@ questions(
   "My name is not Reginald, it is Josh!",
   "My name is Josh!"
 );
-
-// end julio code
-
-// function firstQuestion() {
-//   let firstQuestion = prompt("My name is Reginald. (Y/N)").toUpperCase();
-
-//   if (firstQuestion === "Y" || firstQuestion === "YES") {
-//     alert("My name is not Reginald, it is Josh!");
-//     // console.log('My name is not Reginald, it is Josh!');
-//   } else if (firstQuestion === "N" || firstQuestion === "NO") {
-//     alert("My name is Josh!");
-//   } else {
-//     alert("Please type y/n");
-//   }
-// }
-// firstQuestion();
-
-// function secondQuestion() {
-//   let secondQuestion = prompt(
-//     "Josh used to work at a sushi bar makin damn delicious sushi (Y/N)"
-//   ).toUpperCase();
-
-//   if (secondQuestion === "Y" || secondQuestion === "YES") {
-//     alert("He wishes he knew how to make good sushi");
-//     // console.log(secondQuestion());
-//   } else if (secondQuestion === "N" || secondQuestion === "NO") {
-//     alert("Josh has no clue how to make sushi");
-//     score++;
-//   } else {
-//     alert("Please type y/n");
-//   }
-// }
-// secondQuestion();
-
-// function thirdQuestion() {
-//   let thirdQuestion = prompt(
-//     "Josh really wants to become a gardener after he finishes this coding course (Y/N)"
-//   ).toUpperCase();
-
-//   if (thirdQuestion === "Y" || thirdQuestion === "YES") {
-//     alert("He would prefer to get a job utilizing his newfound skillsets");
-//     // console.log(thirdQuestion());
-//     score++;
-//   } else if (thirdQuestion === "N" || thirdQuestion === "NO") {
-//     alert(
-//       "He would totally pick up gardening once he is working remote 35 hrs/wk makin 120k"
-//     );
-//   } else {
-//     alert("Please type y/n");
-//   }
-// }
-// thirdQuestion();
-
-// function fourthQuestion() {
-//   let fourthQuestion = prompt(
-//     "One of Joshs goals is to get enough experience to get a Software Engineer work title"
-//   ).toUpperCase();
-
-//   if (fourthQuestion === "Y" || fourthQuestion === "YES") {
-//     alert("Golly gee, he would love to earn that title pretty soon");
-//     // console.log(fourthQuestion());
-//     score++;
-//   } else if (fourthQuestion === "N" || fourthQuestion === "NO") {
-//     alert("He would also settle for a Seinor Sofware Engineer");
-//   } else {
-//     alert("Please type y/n");
-//   }
-// }
-// fourthQuestion();
-
-// function fifthQuestion() {
-//   let fifthQuestion = prompt("Do you think Josh likes pizza?").toUpperCase();
-
-//   if (fifthQuestion === "Y" || fifthQuestion === "YES") {
-//     alert("Of course he likes pizza! Who does not?");
-//     // console.log(fifthQuestion());
-//     score++;
-//   } else if (fifthQuestion === "N" || fifthQuestion === "NO") {
-//     alert("Of course he likes pizza!");
-//   } else {
-//     alert("Please type y/n");
-//   }
-// }
-// fifthQuestion();
+questions(
+  "Josh used to work at a sushi bar makin damn delicious sushi (Y/N)",
+  "He wishes he knew how to make good sushi",
+  "Josh has no clue how to make sushi"
+);
+questions(
+  "Josh really wants to become a gardener after he finishes this coding course (Y/N)",
+  "He would prefer to get a job utilizing his newfound skillsets",
+  "He would totally pick up gardening once he is working remote 35 hrs/wk makin 120k"
+);
+questions(
+  "One of Joshs goals is to get enough experience to get a Software Engineer work title (Y/N)",
+  "Golly gee, he would love to earn that title pretty soon",
+  "He would also settle for a Seinor Sofware Engineer"
+);
+questions(
+  "Do you think Josh likes pizza?",
+  "Of course he likes pizza! Who does not?",
+  "Of course he likes pizza!"
+);
 
 // let attempt = 3;
 // let guessNumber = 5;
@@ -158,6 +94,7 @@ questions(
 //   "france",
 //   "china",
 //   "prussia",
+
 // ];
 //while loop controls the game
 // while (userAttempts > 0) {
@@ -183,3 +120,4 @@ questions(
 //     );
 //   }
 // }
+//user();
